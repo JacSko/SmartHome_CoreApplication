f95865f747ca91f2a0d23a62d35e136720ef7f23
JSkowronek <jacekskowronekk@gmail.com>
2020-10-30 21:29:49 +0100

    Initial commit
    Two build targets (STM and UT) are working

---
+++ main.c
@@ -0,0 +1,24 @@
+
+#include "stm32f4xx.h"
+#include "test.h"
+
+
+void led_init()
+{ //PC13
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+}
+
+int main(void)
+{
+
+	uint8_t result = add1(1,2);
+
+//	led_init();
+//	volatile uint32_t delay;
+//	while(1){
+//	GPIOC->ODR |= GPIO_ODR_ODR_13;
+//	for(delay = 1000000; delay; delay--){};
+//	GPIOC->ODR &= ~GPIO_ODR_ODR_13;
+//	for(delay = 1000000; delay; delay--){};
+//	}
+}
---
+++ sw/drivers/source/test.c
@@ -0,0 +1,11 @@
+#include "test.h"
+
+uint8_t add1 (uint8_t arg1, uint8_t arg2)
+{
+	return arg1 + arg2;
+}
+
+uint8_t add2 (uint8_t arg1, uint8_t arg2)
+{
+	return arg1 + arg2;
+}
