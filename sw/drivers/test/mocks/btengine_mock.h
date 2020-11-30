0232de2ad8dcf9d9680de3b560c36515ea3dc029
JSkowronek <jacekskowronekk@gmail.com>
2020-11-30 20:14:37 +0100

    BT engine mock added

---
+++ sw/drivers/test/mocks/btengine_mock.h
@@ -0,0 +1,83 @@
+#ifndef _BTENGINE_MOCK_H_
+#define _BTENGINE_MOCK_H_
+
+#include "bt_engine.h"
+#include "gmock/gmock.h"
+
+struct btEngineMock
+{
+	MOCK_METHOD1(btengine_initialize, RET_CODE(BT_Config*));
+	MOCK_METHOD0(btengine_deinitialize, void());
+	MOCK_METHOD1(btengine_send_string, RET_CODE(const char*));
+	MOCK_METHOD0(btengine_can_read_string, RET_CODE());
+	MOCK_METHOD0(btengine_get_string, const char*());
+	MOCK_METHOD0(btengine_count_bytes, uint16_t());
+	MOCK_METHOD0(btengine_get_bytes, const uint8_t*());
+	MOCK_METHOD0(btengine_string_watcher, void());
+	MOCK_METHOD1(btengine_register_callback, RET_CODE(void(*callback)(const char*)));
+	MOCK_METHOD1(btengine_unregister_callback, RET_CODE(void(*callback)(const char*)));
+};
+
+btEngineMock* btengineMock;
+
+void mock_btengine_init()
+{
+	btengineMock = new btEngineMock;
+}
+
+void mock_btengine_deinit()
+{
+	delete btengineMock;
+}
+
+RET_CODE btengine_initialize(BT_Config* cfg)
+{
+	return btengineMock->btengine_initialize(cfg);
+}
+
+void btengine_deinitialize()
+{
+	btengineMock->btengine_deinitialize();
+}
+
+RET_CODE btengine_send_string(const char * string)
+{
+	return btengineMock->btengine_send_string(string);
+}
+
+RET_CODE btengine_can_read_string()
+{
+	return btengineMock->btengine_can_read_string();
+}
+
+uint16_t btengine_count_bytes()
+{
+	return btengineMock->btengine_count_bytes();
+}
+
+const uint8_t* btengine_get_bytes()
+{
+	return btengineMock->btengine_get_bytes();
+}
+
+const char* btengine_get_string()
+{
+	return btengineMock->btengine_get_string();
+}
+
+void btengine_string_watcher()
+{
+	btengineMock->btengine_string_watcher();
+}
+
+RET_CODE btengine_register_callback(void(*callback)(const char *))
+{
+	return btengineMock->btengine_register_callback(callback);
+}
+
+RET_CODE btengine_unregister_callback(void(*callback)(const char *))
+{
+	return btengineMock->btengine_unregister_callback(callback);
+}
+
+#endif
