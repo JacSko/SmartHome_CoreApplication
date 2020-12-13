8d78932a297a586abb76bcf0a73905787b2ec6ed
JSkowronek <jacekskowronekk@gmail.com>
2020-12-13 18:26:19 +0100

    doxyfile and source/header templated added

---
+++ sw/header_template.h
@@ -0,0 +1,50 @@
+#ifndef HEADER_TEMPLATE_H_
+#define HEADER_TEMPLATE_H_
+
+/* ============================= */
+/**
+ * @file header_template.h
+ *
+ * @brief Oneline brief here
+ *
+ * @details
+ * Longer, multiline description here
+ *
+ * @author Example author
+ * @date 01/01/2020
+ */
+/* ============================= */
+
+/* =============================
+ *          Defines
+ * =============================*/
+
+/* =============================
+ *   Includes of common headers
+ * =============================*/
+
+/* =============================
+ *  Includes of project headers
+ * =============================*/
+
+/* =============================
+ *      Global variables
+ * =============================*/
+
+
+/**
+ * @brief This is method 1
+ * @param[in] arg1 - this is parameter 1
+ * @param[in] arg2 - this is parameter 2
+ * @return It returns something
+ */
+int method1(int arg1, int arg2);
+
+
+
+
+
+
+
+
+#endif
